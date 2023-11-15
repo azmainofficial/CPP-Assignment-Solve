336327e164826322c26ea874222986ff8a78beb4
Azmain Sheikh <123837974+azmainofficial@users.noreply.github.com>
2023-11-15 23:06:06 +0600

    Create Task3.c

---
+++ Arithmetic and Typecasting - 1(orion)/Task3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+    float average;
+
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    average = (float)(num1 + num2 + num3) / 3;
+
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
