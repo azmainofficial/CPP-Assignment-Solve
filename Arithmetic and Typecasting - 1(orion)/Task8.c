ee17d0bcf2a3bccc36b888f9505e9ce622736b59
Azmain Sheikh <123837974+azmainofficial@users.noreply.github.com>
2023-11-15 23:11:17 +0600

    Create Task8.c

---
+++ Arithmetic and Typecasting - 1(orion)/Task8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float base, height, area;
+
+    printf("Enter the base and height of the triangle: ");
+    scanf("%f %f", &base, &height);
+
+    area = 0.5 * base * height;
+
+    printf("Area of the triangle: %.2f\n", area);
+
+    return 0;
+}
