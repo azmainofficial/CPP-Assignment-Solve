f38e8ed61102300eebb07ea9f53667271a882f68
Azmain Sheikh <123837974+azmainofficial@users.noreply.github.com>
2023-11-15 23:10:50 +0600

    Create Task7.c

---
+++ Arithmetic and Typecasting - 1(orion)/Task7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, simpleInterest;
+
+    printf("Enter principal amount, rate of interest, and time: ");
+    scanf("%f %f %f", &principal, &rate, &time);
+
+    simpleInterest = (principal * rate * time) / 100;
+
+    printf("Simple Interest: %.2f\n", simpleInterest);
+
+    return 0;
+}
